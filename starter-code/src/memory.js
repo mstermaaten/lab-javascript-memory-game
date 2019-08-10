737a5a04c39e63fe9d522214c51bf86334525797
mstermaaten <mstermaaten@gmail.com>
2019-08-10 14:54:01 +0200

    semi done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,35 @@
 class MemoryGame {
-  constructor(card){
+  constructor(card) {
     this.cards = cards;
+    this.selectedCards = [];
+    this.rightQuess = 0;
+  }
+
+  checkCards() {
+    const name1 = this.selectedCards[0].dataset.cardname;
+    const name2 = this.selectedCards[1].dataset.cardname;
+    // const anotherName = $(this.selectedCards[0]).data("cardname");
+    if (name1 === name2) {
+      this.rightQuess += 1;
+      console.log(this.rightQuess);
+      this.selectedCards = [];
+    } else {
+      switchCardBack(this.selectedCards[0], this.selectedCards[1]);
+      this.selectedCards = [];
+    }
+    $("#pairs_guessed").text(`${memoryGame.rightQuess}`);
   }
   shuffleCards() {}
-  checkIfPair(card1, card2) {}
   isFinished() {}
+}
+
+function switchCardBack(a, b) {
+  setTimeout(function() {
+    $(a)
+      .children()
+      .toggleClass("front back");
+    $(b)
+      .children()
+      .toggleClass("front back");
+  }, 1000);
 }
